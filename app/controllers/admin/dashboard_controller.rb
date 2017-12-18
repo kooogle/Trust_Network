56cca5a4d77eccda5cc2da0986a23536119d3cd0
Koogle.Zhang <koogle.zhang@gmail.com>
2017-12-18 18:07:22 +0800

    添加图标展示

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,15 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
-
+    per_page = params[:per_page]
+    block = params[:block] || Chain.first.id
+    sta_time = params[:start] || Date.current.to_s
+    end_time = params[:end] || Date.current.to_s
+    @block = Chain.find(block)
+    tickers = @block.markets.where("time_stamp >= ? AND time_stamp <= ?",sta_time.to_time.beginning_of_day.to_i,end_time.to_time.end_of_day.to_i)
+    tickers = tickers.last(per_page.to_i) if per_page.to_i > 0
+    @date_array = tickers.map {|x| x.created_at.strftime('%H:%M')}
+    @last_price = tickers.map {|x| x.bid }
   end
 
 end
