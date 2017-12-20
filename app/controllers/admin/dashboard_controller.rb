9dc1bc07cfdaa46cb5ca01648fdd979e1d0db04e
Koogle.Zhang <koogle.zhang@gmail.com>
2017-12-20 16:59:19 +0800

    添加新版行情图

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,9 +7,8 @@ class Admin::DashboardController < Admin::BaseController
     end_time = params[:end] || Date.current.to_s
     @block = Chain.find(block)
     tickers = @block.markets.where("time_stamp >= ? AND time_stamp <= ?",sta_time.to_time.beginning_of_day.to_i,end_time.to_time.end_of_day.to_i)
-    tickers = tickers.last(per_page.to_i) if per_page.to_i > 0
-    @date_array = tickers.map {|x| x.created_at.strftime('%H:%M')}
-    @last_price = tickers.map {|x| x.bid }
+    @date_array = tickers.map {|x| x.created_at.strftime('%F %H:%M')}
+    @stock_array = tickers.map {|x| x.bid }
   end
 
 end
