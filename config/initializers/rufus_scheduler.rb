cd026f6235b11fa6594e3246db5766b60fc654bb
Koogle.Zhang <koogle.zhang@gmail.com>
2017-12-24 17:36:03 +0800

    时间戳

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -4,8 +4,9 @@ scheduler = Rufus::Scheduler.new
 
 #每一分钟获取一次行情
 scheduler.cron '*/1 * * * *' do
-  time_stamp = Time.now.beginning_of_minute.to_i
-  puts Time.now.to_i
+  time_stamp = Time.now
+  Rails.logger.debug "========#{time_stamp}========"
+  time_stamp = time_stamp.beginning_of_minute.to_i
   Chain.all.each do |block|
     ticker = block.get_market.first
     Market.generate(block.id,ticker,time_stamp) if ticker['MarketName']
