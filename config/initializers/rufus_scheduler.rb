5648cf41452274755ad85f62a1d6a98ed3443c64
Koogle.Zhang <koogle.zhang@gmail.com>
2017-12-24 17:24:04 +0800

    系统时间唯 准确点

--- config/initializers/rufus_scheduler.rb
+++ config/initializers/rufus_scheduler.rb
@@ -5,6 +5,7 @@ scheduler = Rufus::Scheduler.new
 #每一分钟获取一次行情
 scheduler.cron '*/1 * * * *' do
   time_stamp = Time.now.beginning_of_minute.to_i
+  puts Time.now.to_i
   Chain.all.each do |block|
     ticker = block.get_market.first
     Market.generate(block.id,ticker,time_stamp) if ticker['MarketName']
